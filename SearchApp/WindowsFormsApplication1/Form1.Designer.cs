fce68816e67ee7837b69a82896fb51c0474ed8b7
RadhikaKalaiselvan <radhikalaiselvan@gmail.com>
2018-03-23 17:40:32 -0500

    Final version

---
+++ SearchApp/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ignoreCase = new System.Windows.Forms.CheckBox();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchTerm = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.occurrences = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchProgressBar = new System.Windows.Forms.ProgressBar();
+            this.resultsListView = new System.Windows.Forms.ListView();
+            this.Attribute = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Value = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ignoreCase);
+            this.panel1.Controls.Add(this.fileNameTextBox);
+            this.panel1.Controls.Add(this.searchTextBox);
+            this.panel1.Controls.Add(this.searchTerm);
+            this.panel1.Controls.Add(this.searchButton);
+            this.panel1.Controls.Add(this.browseButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(778, 112);
+            this.panel1.TabIndex = 1;
+            // 
+            // ignoreCase
+            // 
+            this.ignoreCase.AutoSize = true;
+            this.ignoreCase.Checked = true;
+            this.ignoreCase.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ignoreCase.Location = new System.Drawing.Point(502, 78);
+            this.ignoreCase.Name = "ignoreCase";
+            this.ignoreCase.Size = new System.Drawing.Size(83, 17);
+            this.ignoreCase.TabIndex = 3;
+            this.ignoreCase.Text = "Ignore Case";
+            this.ignoreCase.UseVisualStyleBackColor = true;
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(83, 22);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(502, 20);
+            this.fileNameTextBox.TabIndex = 0;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(83, 74);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(394, 20);
+            this.searchTextBox.TabIndex = 2;
+            // 
+            // searchTerm
+            // 
+            this.searchTerm.Location = new System.Drawing.Point(4, 74);
+            this.searchTerm.Name = "searchTerm";
+            this.searchTerm.Size = new System.Drawing.Size(73, 23);
+            this.searchTerm.TabIndex = 6;
+            this.searchTerm.Text = "Search for";
+            this.searchTerm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(610, 74);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(157, 22);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(610, 22);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(157, 23);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(4, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.occurrences);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.searchProgressBar);
+            this.panel2.Controls.Add(this.resultsListView);
+            this.panel2.Location = new System.Drawing.Point(12, 118);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(778, 390);
+            this.panel2.TabIndex = 2;
+            // 
+            // occurrences
+            // 
+            this.occurrences.AutoSize = true;
+            this.occurrences.Location = new System.Drawing.Point(585, 330);
+            this.occurrences.Name = "occurrences";
+            this.occurrences.Size = new System.Drawing.Size(169, 13);
+            this.occurrences.TabIndex = 8;
+            this.occurrences.Text = "Total line count with search text: 0";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(15, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Progress Status (%)";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // searchProgressBar
+            // 
+            this.searchProgressBar.Location = new System.Drawing.Point(6, 356);
+            this.searchProgressBar.Name = "searchProgressBar";
+            this.searchProgressBar.Size = new System.Drawing.Size(761, 23);
+            this.searchProgressBar.TabIndex = 1;
+            // 
+            // resultsListView
+            // 
+            this.resultsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Attribute,
+            this.Value});
+            this.resultsListView.Location = new System.Drawing.Point(6, 3);
+            this.resultsListView.Name = "resultsListView";
+            this.resultsListView.ShowItemToolTips = true;
+            this.resultsListView.Size = new System.Drawing.Size(761, 314);
+            this.resultsListView.TabIndex = 0;
+            this.resultsListView.UseCompatibleStateImageBehavior = false;
+            this.resultsListView.View = System.Windows.Forms.View.Details;
+            // 
+            // Attribute
+            // 
+            this.Attribute.Text = "Line Number";
+            this.Attribute.Width = 103;
+            // 
+            // Value
+            // 
+            this.Value.Text = "Line Text";
+            this.Value.Width = 653;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 509);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "SearchForm";
+            this.Text = "Search Application";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView resultsListView;
+        private System.Windows.Forms.ColumnHeader Attribute;
+        private System.Windows.Forms.ColumnHeader Value;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label searchTerm;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar searchProgressBar;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.CheckBox ignoreCase;
+        private System.Windows.Forms.Label occurrences;
+    }
+}
+
---
+++ SearchApp/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+
+
+    public partial class SearchForm : Form
+    {
+
+        String searchText = "";
+        Queue<Content> q;
+        int totalCount;
+        public SearchForm()
+        {
+            InitializeComponent();
+            q = new Queue<Content>();
+            totalCount = 0;
+        }
+
+        /*
+         * When search button is called the following method is invoked.
+         * The following method contains code to toggle between search and cancel operation,
+         * when search is clicked the backgroundworker thread is started.
+         */
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            occurrences.Text = "Total line count with search text: 0";
+            if (searchButton.Text.Equals("Search"))
+            {
+                totalCount = 0;
+                resultsListView.Items.Clear();
+                String filePath = fileNameTextBox.Text;
+                searchText = searchTextBox.Text;
+                String errorMsg = "";
+              
+                //Validate if the two text boxes contain valid values
+                if (filePath.Equals(String.Empty))
+                {
+                    errorMsg += "Enter or select the file path \n";
+                }
+                else if (!filePath.Contains(".txt"))
+                {
+                    errorMsg += "Enter or select a valid .txt file \n";
+                }
+
+
+                if (searchText.Equals(String.Empty))
+                {
+                    errorMsg += "Enter search text";
+                }
+
+                if (!errorMsg.Equals(""))
+                {
+                    MessageBox.Show(errorMsg);
+                }
+                else
+                {
+
+                    if (!File.Exists(filePath))
+                    {
+                        MessageBox.Show("File not found, enter a valid txt file name");
+                    }
+                    else
+                    {
+                        //The selected file is found
+                        //valid case
+                        object[] parameters = new object[4] { filePath, searchText, q, ignoreCase.Checked };
+                        //Change the search button text to cancel
+                        searchButton.Text = "Cancel";
+                        browseButton.Enabled = false;
+                        backgroundWorker1.RunWorkerAsync(parameters);
+                    }
+
+                }
+            }
+            else
+            {
+                //when cancel is called
+                backgroundWorker1.CancelAsync();
+               
+
+            }
+
+        }
+
+        /*
+         * The following method is called when browse button is clicked, it contains code to display the file system files and search for text files
+         */
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Text Files";
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileNameTextBox.Text = openFileDialog1.FileName;
+            }
+        }
+
+        /*
+         *Following code opens the given file and searches for the given search string , if found adds it to the queue along with line number
+         */
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            object[] parameters = e.Argument as object[];
+            string filePath = parameters[0].ToString();
+            string searchText = parameters[1].ToString();
+            Queue<Content> q = (Queue<Content>)parameters[2];
+            bool ignorecase = (bool)parameters[3];
+
+           
+            try
+            {
+                FileStream inFile = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                long fileSizeInBytes = new FileInfo(filePath).Length;
+                long readSize = 0;
+                int lineNum = 0;
+                using (var sr = new StreamReader(inFile))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        if (backgroundWorker1.CancellationPending)
+                        {
+                            //when cancel button sis clicked 
+                            //close the file
+                            e.Cancel = true;
+                            sr.Close();
+                            inFile.Close();
+
+                        }
+                        else
+                        {
+                            var line = sr.ReadLine();
+                            lineNum++;
+
+                            if (String.IsNullOrEmpty(line)) continue;
+                            if (ignorecase && line.IndexOf(searchText, StringComparison.OrdinalIgnoreCase) >= 0)
+                            {
+                                q.Enqueue(new Content(lineNum.ToString().Trim(), line));
+
+                            }
+                            if ((!ignorecase) && line.IndexOf(searchText) >= 0)
+                            {
+
+                                q.Enqueue(new Content(lineNum.ToString().Trim(), line));
+                            }
+
+                            readSize += ASCIIEncoding.ASCII.GetByteCount(line);
+                            int percentage = (int)(((double)readSize / fileSizeInBytes) * 100);
+                            //rounding the completion percentage as discussed in class
+                            if (percentage >= 95)
+                            {
+                                percentage = 100;
+                            }
+                            else if (percentage == 0)
+                            {
+                                percentage = 1;
+                            }
+
+                            backgroundWorker1.ReportProgress(percentage, q);
+                            Thread.Sleep(1);
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        /*
+         * Update the progress status in progress bar
+         */
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            searchProgressBar.Value = e.ProgressPercentage;
+            label2.Text = "Progress Status (" + e.ProgressPercentage.ToString() + "%)";
+            while (q.Count > 0)
+            {
+                Content c = q.Dequeue();
+                totalCount++;
+                ListViewItem item1 = new ListViewItem(c.getLineNumber());
+                item1.SubItems.Add(c.getLineText());
+                resultsListView.Items.Add(item1);
+            }
+            occurrences.Text = "Total line count with search text: " + totalCount;
+
+        }
+
+        /*
+         * When the background worker has completed the task the following method is called,
+         * it reads the Content object from queue and updates the listview in UI and also
+         * updated total lines count with the given search string.
+         * Enable browse button when search is completed or when cancel is clicked.
+         */
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            searchButton.Text = "Search";
+            browseButton.Enabled = true;
+            if (e.Cancelled)
+            {
+               
+               
+                MessageBox.Show("Search was cancelled");
+                occurrences.Text = "Total line count with search text: 0";
+                label2.Text = "Progress Status (0%)";
+                searchProgressBar.Value = 0;
+
+            }
+            else
+            {
+                MessageBox.Show("Successfully completed the search!");
+            }
+            
+        }
+    }
+
+    /*
+     * A class with line number and content used to store in the Queue
+     */
+    public class Content
+    {
+        String lineNumber;
+        String lineText;
+        public Content(String lineNumber, String lineText)
+        {
+            this.lineNumber = lineNumber;
+            this.lineText = lineText;
+        }
+
+        public String getLineNumber()
+        {
+            return lineNumber;
+        }
+
+        public String getLineText()
+        {
+            return lineText;
+        }
+    }
+}
